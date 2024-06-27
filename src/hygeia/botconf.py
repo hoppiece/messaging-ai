3960b024b57f344ce034b2427af9d568c65f8384
Kenta Shinzato <hoppiece@gmail.com>
2024-06-27 21:13:09 +0900

    For production

--- src/hygeia/botconf.py
+++ src/hygeia/botconf.py
@@ -18,12 +18,15 @@ line_bot_api_blob = AsyncMessagingApiBlob(
     AsyncApiClient(Configuration(access_token=settings.LINE_CHANNEL_ACCESS_TOKEN))
 )
 
-dynamodb = boto3.resource(
-    "dynamodb",
-    region_name=settings.AWS_DEFAULT_REGION,
-    endpoint_url=settings.DYNAMODB_ENDPOINT_URL,
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-)
+if settings.DYNAMODB_ENDPOINT_URL is None:  # Prod
+    dynamodb = boto3.resource("dynamodb")
+else:
+    dynamodb = boto3.resource(
+        "dynamodb",
+        region_name=settings.AWS_DEFAULT_REGION,
+        endpoint_url=settings.DYNAMODB_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    )
 
 hygeia_user = dynamodb.Table(settings.TABLE_NAME)
