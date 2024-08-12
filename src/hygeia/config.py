2ee648f4a61f1c97c20b413ea2636a43ceb2426a
Kenta Shinzato <hoppiece@gmail.com>
2024-08-12 17:08:34 +0900

    Cleanup configs

--- src/hygeia/config.py
+++ src/hygeia/config.py
@@ -10,15 +10,10 @@ DOTENV_PATH = os.path.join(os.path.dirname(__file__), "../../.env")
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=DOTENV_PATH)
 
-    LINE_CHANNEL_SECRET: str
-    LINE_CHANNEL_ACCESS_TOKEN: str
+    LINE_CHANNEL_SECRET: str | None = None
+    LINE_CHANNEL_ACCESS_TOKEN: str | None = None
 
     CORS_ALLOW_ORIGIN: str = "*"
-
-    AWS_DEFAULT_REGION: str | None = None
-    AWS_ACCESS_KEY_ID: str | None = None
-    AWS_SECRET_ACCESS_KEY: str | None = None
-    TABLE_NAME: str = "hygeia-user"
 
     MYSQL_SERVER: str = "mysql"
     MYSQL_USER: str
