ba849e9feca8b9f6e90a7f1c5f12b3f141c7f06b
Kenta Shinzato <hoppiece@gmail.com>
2024-08-12 17:08:34 +0900

    Cleanup configs

--- src/hygeia_ai/config.py
+++ src/hygeia_ai/config.py
@@ -7,14 +7,11 @@ DOTENV_PATH = os.path.join(os.path.dirname(__file__), "../../.env")
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=DOTENV_PATH)
-    OPENAI_API_KEY: str
+    OPENAI_API_KEY: str | None = None
 
     # Above does not use in hygeia_ai
     LINE_CHANNEL_SECRET: str | None = None
     LINE_CHANNEL_ACCESS_TOKEN: str | None = None
-    AWS_DEFAULT_REGION: str | None = None
-    AWS_ACCESS_KEY_ID: str | None = None
-    AWS_SECRET_ACCESS_KEY: str | None = None
     MYSQL_SERVER: str | None = None
     MYSQL_USER: str | None = None
     MYSQL_PORT: int | None = None
